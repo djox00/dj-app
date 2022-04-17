6947b2ae61b17c75ed68e6d10bae5d7fa78cb1e1
Djordje Djuric <djordjedjuric05@gmail.com>
2022-04-17 17:43:43 +0200

    more landing page details

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -34,9 +34,18 @@ a();
         <h2><span style={{color: "rgb(180, 64, 200)" }}>AstroDj </span> is designed and created with: </h2>   
          
          <img src={reactfirebase} alt="Firebase" />
+         <div className={styled.components}>  
+         <img src="https://seeklogo.com/images/F/font-awesome-logo-3010FE2434-seeklogo.com.png" alt="fontawesome" /> 
+         <img src="https://camo.githubusercontent.com/973c99d17e4ce72d08c4433449045d8391948711f11ac5f328a585e2a7bc8663/68747470733a2f2f692e696d6775722e636f6d2f515a6f776e68672e706e67" alt="spring" />
+         <img src=" https://mui.com/static/logo.png" alt="mui" />                 
+         <img src="https://camo.githubusercontent.com/84746920d1a9906680c387b3cc8753ee842e996fc8915abd295011e15b594b74/68747470733a2f2f676574626f6f7473747261702e636f6d2f646f63732f352e312f6173736574732f6272616e642f626f6f7473747261702d6c6f676f2d736861646f772e706e67" alt="bootstrap" />  
+         <img src="https://i.pinimg.com/originals/83/03/cd/8303cd6c946f9b0040ade2c6c981157a.jpg" alt="youtube api" /> 
+        
 
+         
 
-         <h2>Project created by: </h2> 
+         </div>
+         <h3>Project created by: </h3> 
 
         <Badge />
         
